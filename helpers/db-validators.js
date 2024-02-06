c74088f0409b803cbda243148465c4d10455754a
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-06 17:37:52 -0600

    Primer push

---
+++ helpers/db-validators.js
@@ -0,0 +1,38 @@
+const Role = require('../models/role');
+const Usuario = require('../models/usuario');
+ 
+const esRoleValido = async (role = '') => {
+    const existeRol = await Role.findOne({role});
+    if(!existeRol){
+        throw new Error(`El role ${ role } no existe en la base de datos`);
+    }
+}
+ 
+const existenteEmail = async (correo = '') => {
+    const existeEmail = await Usuario.findOne({correo});
+    if(existeEmail){
+        throw new Error(`El correo ${ correo } ya esta registrado`);
+    }
+}
+ 
+const existeUsuarioById = async (id = '') => {
+    const existeUsuario = await Usuario.findOne({id});
+    if(existeUsuario){
+        throw new Error(`El id ${id} no pertenede a un usuario`);
+    }
+ 
+}
+
+const existeMascotaById = async (id = '') => {
+    const existeMascota = await Mascota.findOne({id});
+    if(existeMascota){
+        throw new Error(`El id ${id} no pertenece a ninguna mascota`);
+    }
+} 
+module.exports = {
+    esRoleValido,
+    existenteEmail,
+    existeUsuarioById,
+    existeMascotaById
+   
+}
---
+++ models/mascota.js
@@ -0,0 +1,30 @@
+const { Schema, model} = require('mongoose');
+
+const MascotaSchema = Schema({
+    nombre:{
+        type: String,
+        require: [true, 'El nombre de la mascota es obligatorio']
+    },
+    especio:{
+        type: String,
+        require: [true, 'La especie es obligatoria']
+    },
+    edad:{
+        type: String,
+        require: [true, 'La edad es obligatoria']
+    },
+    color:{
+        type: String,
+        require: [true, 'El color es obligatorio']
+    },
+    tamaño:{
+        type: String,
+        require: [true, 'El tamaño es obligatorio']
+    },
+    peso:{
+        type: String,
+        require: [true, 'El peso es obligatorio']
+    }
+});
+
+module.exports = model ('Mascota', MascotaSchema);
